3f4f5d5256161ac0a2dc2f2cbf4253168faef3e4
Liron Toledo <tldlir001@myuct.ac.za>
2018-05-10 15:32:03 +0200

    Minimal changes made, struggling to grasp templates and to read in file correctly

--- Audio.h
+++ Audio.h
@@ -7,6 +7,8 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+# include <cstdint>
+#include <iterator>
 
 #ifndef AUDIOMANIPULATION_AUDIO_H
 #define AUDIOMANIPULATION_AUDIO_H
@@ -19,13 +21,52 @@ namespace tldlir001
     private:
         int sPS; //-r
         int size; //-b
-        int numCh; //-c
+        int channels; //-c
         std::vector<A> data;
 
     public:
-        Audio(int sps, int sz, int nc, std::vector d);
+        template <typename I>
+        Audio(int sps, int sz, int nc, std::string name)
+        {
 
-        void load(std::string name);
+            sPS = sps;
+            size = sz;
+            channels = nc;
+
+            std::ifstream in(name,std::ifstream::binary);
+
+            if(!in)
+            {
+                std::cout << "Couldn't open file" << std::endl;
+            }
+            else
+            {
+                in.seekg (0, in.end);
+                long length = in.tellg();// file size
+                in.seekg (0, in.beg);
+
+                char * buffer = new char [length];
+
+                in.read (buffer,length);
+
+                for (int i = 0; i < length; ++i)
+                {
+                    data[i] = buffer[i];
+                }
+
+
+                std::cout<<data[0] << std::endl;
+                //NumberOfSamples = fileSizeInBytes / (sizeof(intN_t) * channels)
+                //int samples = length/ (sizeof(intN_t)) * channels;
+
+                //deletes buffer from memory
+                delete[] buffer;
+
+            }
+        }
+
+
+
 
 
         A operator+(const A a);
--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
+#include "Audio.h"
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    tldlir001::Audio(44100,16,1,"sample_input/beez18sec_44100_signed_8bit_mono.raw");
+    //sample_input/beez18sec_44100_signed_8bit_mono.raw
+    //std::cout << "Hello, World!" << std::endl;
     return 0;
 }
